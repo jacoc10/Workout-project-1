0b1f21e94c93e3d4528297b42a324bdfdf51a5bd
Jacob <jacoc10@uci.edu>
2024-01-29 23:27:54 -0800

    Changed variable names for clarity and fixed read file function to actually decode the bytes as well as strip the leading white spaces and characters.

--- wp1.py
+++ wp1.py
@@ -13,10 +13,12 @@ def retrieve_inputs():
     return(original_file, encrypted_file)
 
 def read_file(original_file):
-    characters = []
+    file_bytes = []
     with open(original_file, "rb") as f:
         for byte in f:
-            characters.append(byte)
+            file_bytes.append(byte.decode().strip())
+    return(file_bytes)
+
 
 def write_file(content, encrypted_file):
     content.reverse()
